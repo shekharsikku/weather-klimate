6b59124d43643f8ce6056cc6e46f782f050c3236
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-08 20:58:44 +0530

    fix(config): plugin type error and build chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  // @ts-expect-error [plugin type error]
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
