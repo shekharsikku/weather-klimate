fb4256ac39eec9333801ae9b3110fd5c5f25dbfb
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-10 17:03:39 +0530

    fix(type): theme storage key name and details

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,11 +4,22 @@ import Header from "./header";
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
-      <Header/>
-      <main className="min-h-screen container mx-auto px-4 py-8">{children}</main>
-      <footer className="border-t backdrop-blur py-8 supports-[backdrop-filter]:bg-background/60"> 
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur py-8 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-            <p>Made with ❤️</p>
+          <p>
+            Made with ❤️ by{" "}
+            <a
+              href="https://github.com/shekharsikku"
+              target="_blank"
+              className="font-medium"
+            >
+              Shekhar Sharma
+            </a>
+          </p>
         </div>
       </footer>
     </div>
