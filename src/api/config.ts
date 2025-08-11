ed8ba6bcb5e55a582c247c14c42877b4d0128678
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-11 17:12:29 +0530

    refactor(env): openweather url in env variable

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: "https://api.openweathermap.org/data/2.5/",
-  GEO: "http://api.openweathermap.org/geo/1.0",
+  BASE_URL: import.meta.env.VITE_OPENWEATHER_API_BASE_URL,
+  GEO: import.meta.env.VITE_OPENWEATHER_API_GEO_URL,
   API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
   DEFAULT_PARAMS: {
     units: "metric",
