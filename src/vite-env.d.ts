435e7dcd02ba7dc5e00235a4784ab47ab071bb11
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2026-03-11 20:33:53 +0530

    fix(env): variable type in vite env declaration file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  // strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_OPENWEATHER_API_BASE_URL: string;
+  readonly VITE_OPENWEATHER_API_GEO_URL: string;
+  readonly VITE_OPENWEATHER_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
